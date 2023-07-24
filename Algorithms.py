1c7c6ab2aa6f644f3a67e6f044330bb4dd19a3e7
xinshen <50257913+XINS3@users.noreply.github.com>
2023-07-24 09:26:44 +0200

    Add files via upload

---
+++ Algorithms.py
@@ -0,0 +1,370 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+def APWP(edge,p,c):
+    from Poisson_Binomial import P_poisson_binomial_approximation_v1
+    import time
+    '''
+    input:
+    edge: list form : [(1,2),(2,3),(4,5)]
+    p: edge probabilities, list form : [0.2,0.4,0.5]
+    c: clustering, form list / dictionary [[0,3,4],[1,2]] / {[0,3,4],[1,2]}
+    
+    output:
+    approximated expected modularity
+    '''
+   
+
+    # initialize expected modularity
+    
+    S=0
+    
+    #interate cluster (community) by cluster (community)
+    
+    for cluster in c:
+        
+    #edges in the cluster
+    
+        E_c_in=[]
+        
+    #edge possibilities in the cluster
+    
+        Ep_c_in=[]
+        
+    #edges between the cluster
+    
+        E_c_bet=[]
+        
+    #edge possibilities in the cluster
+    
+        Ep_c_bet=[]
+        
+    #edges outside the cluster
+    
+        E_c_out=[]
+        
+    #edge posibilities in the cluster
+    
+        Ep_c_out=[]
+  
+        for i in range(len(edge)):
+            edge_node1=p[i][0]
+            edge_node2=p[i][1]
+            
+            # collect edges completely within community c
+            
+            if edge_node1 in cluster and edge_node2 in cluster:
+                E_c_in.append(edge_position[i])
+                Ep_c_in.append(edge_possibility[i])
+            
+            # collect edges between community c
+            
+            elif edge_node1 in cluster or edge_node2 in cluster:
+                E_c_bet.append(edge_position[i])
+                Ep_c_bet.append(edge_possibility[i])
+                
+            # collect edges outside community c
+            
+            else:
+                E_c_out.append(edge_position[i])
+                Ep_c_out.append(edge_possibility[i])
+        
+        
+        # #edges in ec,ecc_,ec_
+        
+        N_E_c=len(E_c_in)+1
+        N_E_bet=len(E_c_bet)+1
+        N_E_out=len(E_c_out)+1
+        
+        
+        # probabilities of ec, ecc_, ec_ in one partition
+        # use discrete fourier transform to approximate
+        
+        P1=[]
+        for i in range(N_E_c):
+            P1.append(P_poisson_binomial_approximation_v1(i,Ep_c_in))
+        P2=[]
+        for i in range(N_E_bet):
+            P2.append(P_poisson_binomial_approximation_v1(i,Ep_c_bet))
+        P3=[]
+        for i in range(N_E_out):
+            P3.append(P_poisson_binomial_approximation_v1(i,Ep_c_out))
+        
+        Sum=0
+        
+        # calculate expected modularity
+        
+        for i in range(N_E_c):
+            if N_E_c==0:
+                ec=0
+                p1=1
+            else:
+           
+                 ec=i
+               
+                 p1=P1[ec]
+                
+                
+
+            for j in range(N_E_bet):
+                if N_E_bet==0:
+                    ecc_=0
+                    p2=1
+                else:
+                   
+                    ecc_=j
+                   
+                    p2=P2[ecc_]
+                    
+                   
+                   
+                   
+                for k in range(N_E_out):
+                    if N_E_out==0:
+                        ec_=0
+                        p3=1
+                    else:
+                       
+                        ec_=k
+                       
+                        p3=P3[ec_]
+                        
+                       
+                    
+                    # the graph is an empty graph:
+                    
+                    if ec==0 and ecc_==0 and ec_==0:
+                        Sum=0
+                    else:
+                       
+                        Sum+=(ec/((ec+ecc_+ec_))-((2*ec+ecc_)**2/(4*(ec+ecc_+ec_)**2)))*p1*p2*p3
+
+                      
+            
+        S+=Sum   
+        
+    return S
+    
+            
+           
+
+
+# In[37]:
+
+
+
+def PWP(edge,p,c):
+    from Poisson_Binomial import P_poisson_binomial_original_v1
+    import time
+
+    
+
+    # initialize excepted modularity
+    S=0
+    
+    #interate cluster (community) by cluster (community)
+    
+    for cluster in c:
+       
+    #edges in the cluster
+    
+        E_c_in=[]
+        
+    #edge possibilities in the cluster
+    
+        Ep_c_in=[]
+        
+    #edges between the cluster
+    
+        E_c_bet=[]
+        
+    #edge possibilities in the cluster
+    
+        Ep_c_bet=[]
+        
+    #edges outside the cluster
+    
+        E_c_out=[]
+        
+    #edge posibilities in the cluster
+    
+        Ep_c_out=[]
+
+        
+        
+        for i in range(len(edge)):
+            edge_node1=edge[i][0]
+            edge_node2=edge[i][1]
+            
+            # collect edges completely within community c
+            
+            if edge_node1 in cluster and edge_node2 in cluster:
+                E_c_in.append(edge[i])
+                Ep_c_in.append(p[i])
+                
+            # collect edges between community c
+            
+            elif edge_node1 in cluster or edge_node2 in cluster:
+                E_c_bet.append(edge[i])
+                Ep_c_bet.append(p[i])
+                
+            # collect edges outside community c
+            
+            else:
+                E_c_out.append(edge[i])
+                Ep_c_out.append(p[i])
+        
+       
+        # #edges in ec,ecc_,ec_
+        
+        N_E_c=len(E_c_in)+1
+        N_E_bet=len(E_c_bet)+1
+        N_E_out=len(E_c_out)+1
+        
+        # probabilities of ec, ecc_, ec_ in one partition
+        # use definition of poisson binomial distribution
+        
+        P1=[]
+        for i in range(N_E_c):
+            P1.append(P_poisson_binomial_original_v1(i,Ep_c_in))
+        P2=[]
+        for i in range(N_E_bet):
+            P2.append(P_poisson_binomial_original_v1(i,Ep_c_bet))
+        P3=[]
+        for i in range(N_E_out):
+            P3.append(P_poisson_binomial_original_v1(i,Ep_c_out))
+        
+        Sum=0
+        
+        # calculate expected modularity
+        
+        for i in range(N_E_c):
+            if N_E_c==0:
+                ec=0
+                p1=1
+            else:
+           
+                 ec=i
+                
+                 p1=P1[ec]
+                
+            for j in range(N_E_bet):
+                if N_E_bet==0:
+                    ecc_=0
+                    p2=1
+                else:
+                   
+                    ecc_=j
+                   
+                    p2=P2[ecc_]
+                    
+                   
+                  
+                for k in range(N_E_out):
+                    if N_E_out==0:
+                        ec_=0
+                        p3=1
+                    else:
+                       
+                        ec_=k
+                       
+                        p3=P3[ec_]
+                       
+                    
+                    # when the graph is empty
+                    
+                    if ec==0 and ecc_==0 and ec_==0:
+                        Sum=0
+                    else:
+                       
+                        Sum+=(ec/((ec+ecc_+ec_))-((2*ec+ecc_)**2/(4*(ec+ecc_+ec_)**2)))*p1*p2*p3
+                       
+            
+        S+=Sum   
+        
+    return S
+    
+            
+        
+
+
+# In[44]:
+
+
+
+def Trans_C1(c):
+    '''
+    change sequences to clustering
+    c=[0,1,1,3] to c=[[0],[1,2],[3]]
+    this function can be used in PWP and APWP's parameter 'c'
+    
+    input:
+    c: c=[0,1,1,3]
+    node: #nodes (int)
+    
+    output:
+    c_nodes: c=[[0],[1,2],[3]]
+    
+    '''
+
+    c_nodes=[]
+    
+    #tag visit or not
+    
+    c_novisit=[]
+    
+    for i in range(len(c)):
+        
+        # using hash to initialize community to not visit
+        
+        c_novisit.append([c[i],0])
+        
+    for i in range(len(c)):
+        
+        # when community ID not be visited:
+        
+        if c_novisit[i][1]==0:
+            cc=[]
+            cc.append(i)
+            c_novisit[i][1]=-1
+            for j in range(i+1,len(c)):
+                if c[i]==c[j] and c_novisit[j][1]==0:
+                    cc.append(j)
+                    c_novisit[j][1]=-1
+            c_nodes.append(cc)
+    return c_nodes
+
+
+# In[45]:
+
+
+def Trans_C2(c_nodes,node):
+    
+    '''
+    change clustering to sequences
+    c=[[0],[1,2],[3]] to c=[0,1,1,3]
+    this function can be used in brute force and sampling's parameter 'c'
+    
+    input:
+    c_nodes: c=[[0],[1,2],[3]]
+    node: #nodes (int)
+    
+    output:
+    c: c=[0,1,1,3]
+    
+    '''
+    
+    c=np.zeros(node)
+    for i in range(len(c_nodes)):
+        
+        
+        for j in range(len(c_nodes[i])):
+            c[list(c_nodes[i])[j]]=int(i)
+      
+        
+    return c
+
---
+++ Factors influencing execution time (4.3).py
@@ -0,0 +1,632 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Forest Fire Network Model
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+import random
+import time
+
+# ------------------------------------
+from Classic_Sampling import classic_sampling
+from Brute_Force import brute_force
+from Algorithms import APWP,PWP, Trans_C1,Trans_C2
+from Probabilities import PB,DFT
+from Network_Generator import rand_graph, rand_graph_com,entropy_
+from Communities import Random_Community,SizeVariance_Commuinity
+from Threshold import threshold
+from Save_Load import save_graph,load_graph,save_com,load_com
+
+
+# In[2]:
+
+
+# read edges of FFN 
+#filepath='/Users/.../Desktop/'
+filename='ffn.csv'
+f=open(filename,"r")
+lines=f.readlines()
+edge_position_ffn=[]
+
+for x in lines:
+    node1=int(x.split(' ')[0])
+    node2=int(x.split(' ')[1].split('\n')[0])
+ 
+    edge_position_ffn.append((node1,node2))
+    
+f.close()
+
+
+# In[5]:
+
+
+#generate edge probabilities
+entropy=0.4
+edge_possibility_FFN=entropy_(entropy*len(edge_position_ffn),len(edge_position_ffn))
+print(len(edge_possibility_FFN[0]))
+edge_possibility_FFN=edge_possibility_FFN[0]
+
+
+# In[7]:
+
+
+import csv
+from collections import defaultdict
+
+columns = defaultdict(list) # each value in each column is appended to a list
+
+#open community file to control the number of clusters
+
+with open('cluster_ffn_c=6.csv') as f:
+    reader = csv.DictReader(f) # read rows into a dictionary format
+    for row in reader: # read a row as {column1: value1, column2: value2,...}
+        for (k,v) in row.items(): # go over each column name and value 
+            columns[k].append(v) # append the value into the appropriate list
+                                 # based on column name k
+
+print(columns['Id'])
+print(columns['modularity_class'])
+print(len(columns['Id']))
+
+
+# In[8]:
+
+
+c=[]
+for i in range(len(columns['Id'])):
+    c.append([int(columns['Id'][i]),int(columns['modularity_class'][i])])
+c=sorted(c,key=lambda l:l[1])
+
+c
+com=[]
+front_point=0
+c1=[]
+c1.append(c[0][0])
+for i in range(1,len(c)):
+    
+    later_point=i
+    #print(front_point,later_point)
+    if c[later_point][1]==c[front_point][1]:
+        c1.append(c[later_point][0])
+        
+    else:
+        com.append(c1)
+        
+       
+        c1=[]
+        c1.append(c[later_point][0])
+    
+    if later_point==len(c)-1:
+        
+        com.append(c1)
+        
+        
+    front_point=later_point
+    
+ss=0
+for i in com:
+    ss+=len(i)
+print(ss)
+print('N_cluster',len(com))
+
+
+# In[9]:
+
+
+#shuffle edges
+
+random.shuffle(edge_possibility_FFN)
+print(edge_possibility_FFN)
+plt.plot(edge_possibility_FFN)
+
+
+# In[11]:
+
+
+#Calculate
+
+import time
+start1=time.time()
+s1=APWP(edge_position_ffn,edge_possibility_FFN,com)#older version com is wrong
+end1=time.time()
+print(s1,end1-start1)
+
+
+# # Barabási-Abert
+
+# In[13]:
+
+
+# read edges
+#filepath='/Users/.../Desktop/'
+filename='ba.csv'
+f=open(filename,"r")
+lines=f.readlines()
+edge_position_BA=[]
+for x in lines:
+    node1=int(x.split(' ')[0])
+    node2=int(x.split(' ')[1].split('\n')[0])
+    edge_position_BA.append((node1,node2))
+    
+f.close()
+edge_position_BA
+
+
+# In[ ]:
+
+
+len(edge_position_BA)
+
+
+# In[14]:
+
+
+import csv
+from collections import defaultdict
+
+columns = defaultdict(list) # each value in each column is appended to a list
+
+#change file to control number of communities
+
+with open('cluster_ba_c=5.csv') as f:
+    reader = csv.DictReader(f) # read rows into a dictionary format
+    for row in reader: # read a row as {column1: value1, column2: value2,...}
+        for (k,v) in row.items(): # go over each column name and value 
+            columns[k].append(v) # append the value into the appropriate list
+                                 # based on column name k
+
+print(len(columns['Id']))
+print(columns['modularity_class'])
+
+
+# In[15]:
+
+
+c=[]
+for i in range(len(columns['Id'])):
+    c.append([int(columns['Id'][i]),int(columns['modularity_class'][i])])
+c=sorted(c,key=lambda l:l[1])
+
+c
+com=[]
+front_point=0
+c1=[]
+c1.append(c[0][0])
+for i in range(1,len(c)):
+    
+    later_point=i
+    #print(front_point,later_point)
+    if c[later_point][1]==c[front_point][1]:
+        c1.append(c[later_point][0])
+        
+    else:
+        com.append(c1)
+        
+       
+        c1=[]
+        c1.append(c[later_point][0])
+    
+    if later_point==len(c)-1:
+        
+        com.append(c1)
+        
+        
+    front_point=later_point
+    
+ss=0
+for i in com:
+    ss+=len(i)
+print(ss)
+print('cluster',len(com))
+
+
+# In[16]:
+
+
+#To control the edge probabilities in BA is similar in FFN
+
+edge_possibility_BA=[]
+for i in range(len(edge_position_BA)):
+    edge_possibility_BA.append(edge_possibility_FFN[i])
+
+
+# In[17]:
+
+
+import time
+start1=time.time()
+s1=APWP(edge_position_BA,edge_possibility_BA,com)
+end1=time.time()
+print(s1,end1-start1)
+
+
+# # ER model
+
+# In[18]:
+
+
+# read edges
+#filepath='/Users/.../Desktop/'
+filename='er.csv'
+f=open(filename,"r")
+lines=f.readlines()
+edge_position_ER=[]
+for x in lines:
+    node1=int(x.split(' ')[0])
+    node2=int(x.split(' ')[1].split('\n')[0])
+    edge_position_ER.append((node1,node2))
+    
+f.close()
+
+
+# In[19]:
+
+
+import csv
+from collections import defaultdict
+
+columns = defaultdict(list) # each value in each column is appended to a list
+
+with open('cluster_er_c=5.csv') as f:
+    reader = csv.DictReader(f) # read rows into a dictionary format
+    for row in reader: # read a row as {column1: value1, column2: value2,...}
+        for (k,v) in row.items(): # go over each column name and value 
+            columns[k].append(v) # append the value into the appropriate list
+                                 # based on column name k
+
+print(columns['Id'])
+print(columns['modularity_class'])
+print(len(columns['Id']))
+
+
+# In[20]:
+
+
+c=[]
+for i in range(len(columns['Id'])):
+    c.append([int(columns['Id'][i]),int(columns['modularity_class'][i])])
+c=sorted(c,key=lambda l:l[1])
+
+c
+com=[]
+front_point=0
+c1=[]
+c1.append(c[0][0])
+for i in range(1,len(c)):
+    
+    later_point=i
+    #print(front_point,later_point)
+    if c[later_point][1]==c[front_point][1]:
+        c1.append(c[later_point][0])
+        
+    else:
+        com.append(c1)
+        
+       
+        c1=[]
+        c1.append(c[later_point][0])
+    
+    if later_point==len(c)-1:
+        
+        com.append(c1)
+        
+        
+    front_point=later_point
+    
+ss=0
+for i in com:
+    ss+=len(i)
+print(ss)
+print('cluster',len(com))
+
+
+# In[21]:
+
+
+#To control edge probabilities in ER is similar in FFN and BA
+
+edge_possibility_ER=[]
+for i in range(len(edge_possibility_FFN)):
+    edge_possibility_ER.append(edge_possibility_FFN[i])
+while(len(edge_possibility_ER)<len(edge_position_ER)):
+    edge_possibility_ER.append(random.random())
+    
+
+
+# In[22]:
+
+
+import time
+start1=time.time()
+s1=APWP(edge_position_ER,edge_possibility_ER,com)
+end1=time.time()
+print(s1,end1-start1)
+
+
+# # Small world
+
+# In[23]:
+
+
+# read edges
+#filepath='/Users/.../Desktop/'
+filename='sw.csv'
+f=open(filename,"r")
+lines=f.readlines()
+edge_position_SW=[]
+for x in lines:
+    node1=int(x.split(' ')[0])
+    node2=int(x.split(' ')[1].split('\n')[0])
+    edge_position_SW.append((node1,node2))
+    
+f.close()
+
+
+# In[24]:
+
+
+# To control edge probabilities in SW is similar in ER, FFN, and BA
+edge_possibility_SW=[]
+for i in edge_possibility_ER:
+    edge_possibility_SW.append(i)
+
+
+# In[25]:
+
+
+import csv
+from collections import defaultdict
+
+columns = defaultdict(list) # each value in each column is appended to a list
+
+with open('cluster_sw_c=5.csv') as f:
+    reader = csv.DictReader(f) # read rows into a dictionary format
+    for row in reader: # read a row as {column1: value1, column2: value2,...}
+        for (k,v) in row.items(): # go over each column name and value 
+            columns[k].append(v) # append the value into the appropriate list
+                                 # based on column name k
+
+print(columns['Id'])
+print(columns['modularity_class'])
+
+
+# In[26]:
+
+
+c=[]
+for i in range(len(columns['Id'])):
+    c.append([int(columns['Id'][i]),int(columns['modularity_class'][i])])
+c=sorted(c,key=lambda l:l[1])
+
+c
+com=[]
+front_point=0
+c1=[]
+c1.append(c[0][0])
+for i in range(1,len(c)):
+    
+    later_point=i
+    #print(front_point,later_point)
+    if c[later_point][1]==c[front_point][1]:
+        c1.append(c[later_point][0])
+        
+    else:
+        com.append(c1)
+        
+       
+        c1=[]
+        c1.append(c[later_point][0])
+    
+    if later_point==len(c)-1:
+        
+        com.append(c1)
+        
+        
+    front_point=later_point
+    
+ss=0
+for i in com:
+    ss+=len(i)
+print(ss)
+print('cluster',len(com))
+
+
+# In[27]:
+
+
+import time
+start1=time.time()
+s1=APWP(edge_position_SW,edge_possibility_SW,com)
+end1=time.time()
+print(s1,end1-start1)
+
+
+# # CCS graph
+
+# In[ ]:
+
+
+import networkx as nx
+import matplotlib.pyplot as plt 
+
+
+# C=5
+
+# In[ ]:
+
+
+k=5
+l=40
+p_in=0.111
+p_out=0.01
+g_ffn=nx.planted_partition_graph(k,l,p_in,p_out)
+print(len(g_ffn.edges),len(g_ffn.nodes))
+#nx.draw(g)
+
+
+# In[ ]:
+
+
+edge_position_c_5=[]
+   
+for m in nx.edges(g_ffn):
+        edge_position_c_5.append(m)
+
+
+# In[ ]:
+
+
+com=nx.community.louvain_communities(g_ffn)
+len(com)
+
+
+# In[ ]:
+
+
+#To control edge probabilities similar to SW, BA, FFN, and ER
+
+edge_pos_c_5=[]
+for i in edge_possibility_SW:
+    edge_pos_c_5.append(i)
+while(len(edge_pos_c_5)<len(edge_position_c_5)):
+    edge_pos_c_5.append(random.random())
+
+
+# In[ ]:
+
+
+import time
+start1=time.time()
+s1=APWP(edge_position_c_5,edge_pos_c_5,com)
+end1=time.time()
+print(s1,end1-start1)
+
+
+# C=4
+
+# In[ ]:
+
+
+k=4
+l=50
+p_in=0.09
+p_out=0.01
+g_ba=nx.planted_partition_graph(k,l,p_in,p_out)
+
+print(len(g_ba.edges),len(g_ba.nodes))
+
+
+# In[ ]:
+
+
+edge_position_c_ba=[]
+   
+for m in nx.edges(g_ba):
+        edge_position_c_ba.append(m)
+edge_possibility_c_ba=[]
+for i in range(len(edge_position_c_ba)):
+    edge_possibility_c_ba.append(edge_possibility_ER[i])
+    
+
+
+# In[ ]:
+
+
+com=nx.community.louvain_communities(g_ba)
+len(com)
+
+
+# In[ ]:
+
+
+import time
+start1=time.time()
+s1=APWP(edge_position_c_ba,edge_possibility_c_ba,com)
+end1=time.time()
+print(s1,end1-start1)
+
+
+# C=6
+
+# In[ ]:
+
+
+k=6
+l=34
+p_in=0.13
+p_out=0.01
+g_sw=nx.planted_partition_graph(k,l,p_in,p_out)
+
+len(g_sw.edges)
+
+
+# In[ ]:
+
+
+com=nx.community.louvain_communities(g_sw)
+len(com)
+
+
+# In[ ]:
+
+
+edge_position_c_sw=[]
+   
+for m in nx.edges(g_sw):
+        edge_position_c_sw.append(m)
+
+    
+
+
+# In[ ]:
+
+
+import time
+start1=time.time()
+s1=APWP(edge_position_c_sw,edge_possibility_SW,com)
+end1=time.time()
+print(s1,end1-start1)
+
+
+# In[ ]:
+
+
+from matplotlib.patches import Rectangle
+species = ("C=4", "C=5", "C=6")
+
+width=0.25
+patterns = [ "/" , "\\" , "|" , "-" , "+" , "x", "o", "O", ".", "*" ]
+fig, ax = plt.subplots()
+x = np.arange(len(species)) 
+
+
+
+re1=ax.bar(x+0.1,[195.023,240.317,231.256],width=0.12,label='FFN',hatch='///',color='white',edgecolor='black')
+ax.bar_label(re1,size=8)
+#re=ax.bar( x+0.1, [161.576,298.008,280.12,287.372], width=0.25,label=[i for i in ['FFN','BA','ER','SW']],hatch=["/" , "-" , "\\" , "x"],color='white',edgecolor='black');
+#ax.bar_label(re,size=10)
+re2=ax.bar(x+0.25,[76.766,122.843,159.968],width=0.12,label='BA',hatch='|||',color='white',edgecolor='black')
+ax.bar_label(re2,size=8)
+re3=ax.bar(x+0.4,[82.754,122.283,153.788],width=0.12,label='ER',hatch='--',color='white',edgecolor='black')
+ax.bar_label(re3,size=8)
+re4=ax.bar(x+0.55,[97.522,140.291,176.599],width=0.12,label='SW',hatch='xx',color='white',edgecolor='black')
+ax.bar_label(re4,size=8)
+re5=ax.bar(x+0.7,[86.052,125.015,163.547],width=0.12,label='CCS graph',hatch='..',color='white',edgecolor='black')
+ax.bar_label(re5,size=8)
+
+
+ax.set_xticks(x + width, species,size=15)
+plt.yticks(fontsize=15)
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
+          fancybox=True, shadow=True, ncol=5,frameon=False,fontsize=15)
+ax.set_ylabel('Time (s)',fontsize=15)
+ax.set_title('Computation time in different networks',fontsize=15)
+fig.set_figheight(6)
+fig.set_figwidth(10)
+plt.savefig('/.../partition_time.pdf')
+
